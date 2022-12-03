2ddedb60768885780d71a95680a28c3b2e10c488
aldinhalilovic <aldinhalilovic06@gmail.com>
2022-12-03 21:53:14 +0100

    slider

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,8 +1,15 @@
 import React from "react";
 import styled from "styled-components";
+import ImgSlider from "./ImgSlider";
+import Viewers from "./Viewers";
 
 const Home = () => {
-  return <Container>Home</Container>;
+  return (
+    <Container>
+      <ImgSlider />
+      <Viewers />
+    </Container>
+  );
 };
 
 export default Home;
@@ -11,10 +18,10 @@ const Container = styled.main`
   min-height: calc(100vh - 70px);
   padding: 0 calc(3.5vw + 5px);
   position: relative;
+  overflow-x: hidden;
 
   &::before {
-    background: url("/images/home-background.png") center center / cover
-      no-repeat fixed;
+    background: url("/images/home-background.png") center center / cover no-repeat fixed;
     content: "";
     position: absolute;
     top: 0;
